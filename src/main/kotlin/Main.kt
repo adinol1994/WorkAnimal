90163f27df0b83744c2795266cb5efca0da32046
Roman <119092269+adinol1994@users.noreply.github.com>
2022-12-01 15:15:03 +0900

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,44 +1,8 @@
 fun main(args: Array<String>) {
 
-    val appDataBaseMock = AppDataBaseMock()
-
+ val appDataBaseMock= AppDataBaseMock()
     val repository = AnimalRepositoryRepository(appDataBaseMock)
-
-    println(repository.getAll())
-
-    val animal = repository.findById(4)
-    println(animal)
-    if (animal != null) {
-        repository.delete(animal)
+    val animals = repository.getAll()
+    animals.forEach{
+        animal->println(animal)
     }
-    println(repository.getAll())
-
-
-    val carnivores1 = Carnivores(9, "cat", 42, "S124")
-    val carnivores2 = Carnivores(6, "dog", 50, "P123")
-
-    val Herbivores1 = Herbivores(9, "cat", 42, "S124")
-    val Herbivores2 = Herbivores(6, "dog", 50, "P123")
-
-
-    val list1 = mutableListOf<Animal>()
-    list1.add(carnivores1)
-
-    val list2 = mutableListOf<Animal>()
-    list2.add(carnivores2)
-
-
-
-
-}
-
-private fun <E> MutableList<E>.add(element: Unit) {
-
-}
-
-fun Carnivores1(id: Int, name: String, speeds: Int, group: String) {}
-fun Carnivores2(id: Int, name: String, speeds: Int, group: String){
-
-}
-
-
